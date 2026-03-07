b0c6c1ed505a228959fd8e6cf47fb57c7c4391be
Brenda-Kethleen <87777788+Brenda-Kethleen@users.noreply.github.com>
2026-03-07 19:57:25 -0300

    criando botões

--- Calculadora/app/_layout.tsx
+++ Calculadora/app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Text } from "@react-navigation/elements";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Button } from "react-native";
 
 export default function RootLayout() {
+
+  const linhas = [
+    ["7","8","9","/"],
+    ["4","5","6","x"],
+    ["1","2","3","-"],
+    ["0","√","=","+"]
+  ]
 
   const [soma, setSoma] = useState(1)
   const [subtracao, setsubtração] = useState(100)
@@ -35,10 +41,45 @@ export default function RootLayout() {
 
   return (
     <>
-    <SafeAreaView>
-    
-      <Text>Calculadora</Text>
-      <Text>Resultado:</Text>
+    <SafeAreaView style={{ padding: 20 }}>
+    <Text style={{ fontSize: 30 }}>Calculadora</Text>
+
+      {linhas.map((linha, indexLinha) => (
+
+        <View
+          key={indexLinha}
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginVertical: 5
+          }}
+        >
+
+          {linha.map((botao, indexBotao) => (
+
+            <TouchableOpacity
+              key={indexBotao}
+              style={{
+                backgroundColor: "#b4b4ad",
+                width: 70,
+                padding: 20,
+                borderRadius: 10,
+                alignItems: "center"
+              }}
+            >
+
+              <Text style={{ color: "white", fontSize: 20 }}>
+                {botao}
+              </Text>
+
+            </TouchableOpacity>
+
+          ))}
+
+        </View>
+
+      ))}
+
       <Text>Soma: {soma}</Text>
       <Text>Subtração: {subtracao}</Text>
       <Text>Multiplicação: {multiplicacao}</Text>
@@ -111,10 +152,12 @@ export default function RootLayout() {
       }}
       >
       <Text style={{ color: "white" }}>√</Text>
+
+      
       </TouchableOpacity>
 
       
-
+      
 
     
     
